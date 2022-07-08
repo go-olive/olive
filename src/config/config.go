b8ef6817fa52987432f7892c2ad5b634a33e3dc6
luxcgo <73606679+luxcgo@users.noreply.github.com>
2022-07-09 01:28:05 +0800

    config: flag -u (#29)

--- src/config/config.go
+++ src/config/config.go
@@ -10,14 +10,33 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	l "github.com/go-olive/olive/src/log"
+	"github.com/go-olive/tv"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 var (
 	AppVersion string
-	APP        *appConfig
-	defaultAPP = appConfig{}
+	APP        = &appConfig{}
+	defaultAPP = &appConfig{
+		LogLevel:          logrus.DebugLevel,
+		SnapRestSeconds:   15,
+		CommanderPoolSize: 1,
+		UploadConfig:      &UploadConfig{},
+		PlatformConfig: &PlatformConfig{
+			DouyinCookie: "__ac_nonce=062c84d05004a461cf7f2; __ac_signature=_02B4Z6wo00f01NAKk1QAAIDBqMR4UNttsUjQKpfAAFbQjTrG-JmICfTUMMVzKe3crg5Fk4y4e4DGURjAV4VW2B6WwXdqq3UC1c0waQMKIjhZn5Ve1LxiGmyDuVlSBN7aRhuGfEIwwfxXcYhA4e;",
+		},
+	}
+
+	appCfgFilePath string
+	version        bool
+	url            string
+	cookie         string
+	usage          = func() {
+		fmt.Printf("Powered by go-olive/olive %s\n", AppVersion)
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+	}
 )
 
 type appConfig struct {
@@ -32,13 +51,25 @@ type appConfig struct {
 
 func (this *appConfig) checkAndFix() {
 	if this.LogLevel == 0 {
-		this.LogLevel = logrus.DebugLevel
+		this.LogLevel = defaultAPP.LogLevel
 	}
 	if this.SnapRestSeconds == 0 {
-		this.SnapRestSeconds = 15
+		this.SnapRestSeconds = defaultAPP.SnapRestSeconds
 	}
 	if this.CommanderPoolSize == 0 {
-		this.CommanderPoolSize = 1
+		this.CommanderPoolSize = defaultAPP.CommanderPoolSize
+	}
+	if this.UploadConfig == nil {
+		this.UploadConfig = &UploadConfig{}
+	}
+	if this.PlatformConfig == nil {
+		this.PlatformConfig = &PlatformConfig{}
+	}
+	if this.DouyinCookie == "" {
+		this.DouyinCookie = defaultAPP.DouyinCookie
+	}
+	if cookie != "" {
+		this.DouyinCookie = cookie
 	}
 }
 
@@ -78,61 +109,58 @@ type PlatformConfig struct {
 }
 
 func init() {
-	var (
-		appCfgFilePath string
-		version        bool
-	)
 	flag.BoolVar(&version, "v", version, "print olive version")
 	flag.StringVar(&appCfgFilePath, "c", appCfgFilePath, "set config.toml filepath")
+
+	flag.StringVar(&url, "u", url, "room url")
+	flag.StringVar(&cookie, "sc", "", "site cookie")
+
 	flag.Parse()
 
 	if version {
 		fmt.Println(AppVersion)
 		os.Exit(0)
-	}
+	} else if url != "" {
+		t, err := tv.NewWithUrl(url, tv.SetCookie(cookie))
+		if err != nil {
+			l.Logger.Fatal(err)
+		}
+		site, _ := tv.Sniff(t.SiteID)
+		APP.Shows = []*Show{
+			{
+				StreamerName: site.Name(),
+				Platform:     t.SiteID,
+				RoomID:       t.RoomID,
+			},
+		}
+		APP.verify()
+	} else {
+		if appCfgFilePath == "" {
+			usage()
+			os.Exit(0)
+		}
 
-	var Usage = func() {
-		fmt.Printf("Powered by go-olive/olive %s\n", AppVersion)
-		fmt.Println("Usage:")
-		flag.PrintDefaults()
-	}
-	if appCfgFilePath == "" {
-		Usage()
-		os.Exit(0)
-	}
-
-	viper.SetConfigFile(appCfgFilePath)
-	if err := viper.ReadInConfig(); err != nil {
-		l.Logger.WithField("err", err.Error()).
-			Fatal("load config file failed")
-	}
-	viper.Unmarshal(&APP)
-	verify()
-
-	viper.OnConfigChange(func(e fsnotify.Event) {
+		viper.SetConfigFile(appCfgFilePath)
+		if err := viper.ReadInConfig(); err != nil {
+			l.Logger.WithField("err", err.Error()).
+				Fatal("load config file failed")
+		}
 		viper.Unmarshal(&APP)
-		verify()
-	})
-	viper.WatchConfig()
+		APP.verify()
+
+		viper.OnConfigChange(func(e fsnotify.Event) {
+			viper.Unmarshal(&APP)
+			APP.verify()
+		})
+		viper.WatchConfig()
+	}
 }
 
-func verify() {
-	if APP == nil {
-		l.Logger.Info("use default APP config")
-		APP = &defaultAPP
-	}
-	APP.checkAndFix()
+func (this *appConfig) verify() {
+	this.checkAndFix()
+	l.Logger.SetLevel(this.LogLevel)
 
-	l.Logger.SetLevel(APP.LogLevel)
-
-	if APP.UploadConfig == nil {
-		APP.UploadConfig = &UploadConfig{}
-	}
-	if APP.PlatformConfig == nil {
-		APP.PlatformConfig = &PlatformConfig{}
-	}
-
-	for _, s := range APP.Shows {
+	for _, s := range this.Shows {
 		s.checkAndFix()
 		if s.Parser == "flv" {
 			continue
@@ -143,8 +171,8 @@ func verify() {
 		}
 	}
 
-	if APP.UploadConfig.Enable {
-		if _, err := exec.LookPath(APP.UploadConfig.ExecPath); err != nil {
+	if this.UploadConfig.Enable {
+		if _, err := exec.LookPath(this.UploadConfig.ExecPath); err != nil {
 			l.Logger.Fatal("biliup needs to be installed first")
 		}
 		path, err := os.Getwd()
